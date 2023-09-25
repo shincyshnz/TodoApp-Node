6de5536dd3c7bc62ccb452d56f387f2680f90fc2
Shincy <shincyshnz@gmail.com>
2023-09-25 21:25:36 +0530

    edited mongoose.connect

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
-        const { connection } = await mongoose.connect(process.env.MONGO_URI);
+        const { connection } = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: true,
+            useCreateIndex: true
+        });
         console.log("Database Connected:" + connection.host);
     } catch (error) {
         console.log(error);
