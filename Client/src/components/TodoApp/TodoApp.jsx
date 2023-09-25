d772234272b033723499f95b495fa8c528952b58
Shincy <shincyshnz@gmail.com>
2023-09-25 21:13:04 +0530

    edited API_URL in client

--- Client/src/components/TodoApp/TodoApp.jsx
+++ Client/src/components/TodoApp/TodoApp.jsx
@@ -8,7 +8,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 // const API_URL = "http://localhost:3010/api/task";
-const API_URL = "https://todo-app-node-server-db.vercel.app/";
+const API_URL = "https://todo-app-node-server-db.vercel.app/api/task";
 
 export const TodoApp = () => {
   const inputRef = useRef(null);
