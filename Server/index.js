86e3981096aebb53a457e96a28341605191d03a7
Shincy <shincyshnz@gmail.com>
2023-09-25 20:38:42 +0530

    Menu mobile view edited

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { v4: uuidv4 } = require('uuid');
-require('dotenv').config()
+require('dotenv').config();
 const connectDb = require("./config/db");
 const taskRoute = require("./routes/taskRoutes");
 
